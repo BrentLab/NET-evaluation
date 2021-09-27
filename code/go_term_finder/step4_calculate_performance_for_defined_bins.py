a291531008b776ee39744228adb1a7c128b5cfe8
Dhoha <abid.dhoha@gmail.com>
2021-09-27 11:47:37 -0500

    add multiprocessing when writing files, take abs value of net score before evaluation, add data flag, upgrade singularity package to 3.6 instead of 2.4, fix bug with binding evaluation

--- code/go_term_finder/step4_calculate_performance_for_defined_bins.py
+++ code/go_term_finder/step4_calculate_performance_for_defined_bins.py
@@ -1,38 +1,46 @@
+def collect_l_pvalue(p_in_go_file):
+    from pandas import read_csv
+    
+    df_go = read_csv(p_in_go_file, header=0, sep='\t')
+    df_go_pvalue = df_go.sort_values(ascending=True, by='CORRECTED P-VALUE').loc[:, 'CORRECTED P-VALUE']
+    pvalue = df_go_pvalue.iloc[0]
+    return pvalue
+            
+
 def calculate_performance_for_defined_bins(p_net
                                            , p_dir_results
                                            , l_nbr_edges_per_reg
                                            , p_eval):
-    from os import listdir
-    from pandas import Series, read_csv, DataFrame
-    import numpy as np
     
-    # get the set of regulators
+    from pandas import Series, read_csv, DataFrame
+    from statistics import mean, median
+    import numpy as np
+    import multiprocessing as mp
+    from os import listdir
+    
+    # get the set of regulators after reading network
     df_net = read_csv(p_net, header=None, sep='\t')
     df_net.columns = ['REGULATOR', 'TARGET', 'VALUE']
     nbr_reg = len(set(list(df_net.loc[:, 'REGULATOR'])))
     
-    l_score, l_score_adjusted, l_sum_pvalue, l_nbr_tf = [], [], [], []  # list of performance score
+    pool = mp.Pool(40)
     
     # loop over bins and calculate sum(-log(pvalue))
+    l_sum_pvalue, l_mean_pvalue, l_median_pvalue, l_nbr_tf = [], [], [], []  # list of performance score
     for nbr_edges_per_reg in l_nbr_edges_per_reg:
-        print(nbr_edges_per_reg)
-        sum_pvalue, nbr_tf = 0, 0
         p_dir = p_dir_results + 'bin_' + str(nbr_edges_per_reg) + '/'
-        for tf_file in listdir(p_dir):
-            if tf_file.endswith('.tsv'):
-                df_go = read_csv(p_dir + tf_file, header=0, sep='\t')
-                df_go_pvalue = df_go.sort_values(ascending=True, by='CORRECTED P-VALUE').loc[:, 'CORRECTED P-VALUE']
-                if df_go_pvalue.shape[0] > 0:
-                    pvalue = df_go_pvalue.iloc[0]
-                    if not np.isnan(pvalue):
-                        sum_pvalue-=np.log(pvalue)
-                        nbr_tf += 1 
-        l_score.append(sum_pvalue/(nbr_edges_per_reg*nbr_reg))
-        l_score_adjusted.append(sum_pvalue/nbr_tf)
-        l_sum_pvalue.append(sum_pvalue)
-        l_nbr_tf.append(nbr_tf)
+        l_pvalue = pool.starmap(collect_l_pvalue, [(p_dir + tf_file, ) for tf_file in listdir(p_dir) if tf_file.endswith('.tsv')])
+        # calcualte stats
+        l_sum_pvalue.append(sum(-np.log(l_pvalue)))
+        l_mean_pvalue.append(mean(-np.log(l_pvalue)))
+        l_median_pvalue.append(median(-np.log(l_pvalue)))
+        l_nbr_tf.append(len(l_pvalue))
+    pool.close()
+    pool.join()
+        
+        
 
-    DataFrame([l_score, l_score_adjusted, l_sum_pvalue, l_nbr_tf], index=['score', 'score_adjusted', 'sum_pvalue', 'nbr_tf']).T.to_csv(p_eval, header=True, index=False, sep='\t')       
+    DataFrame([l_sum_pvalue, l_mean_pvalue, l_median_pvalue, l_nbr_tf], index=['sum', 'mean', 'median', 'nbr_tf']).T.to_csv(p_eval, header=True, index=False, sep='\t')       
 
 
 def main():
